dc4dd2441fd66fca05d76918323bbc0b661c61bd
mariusz <mariusz@localhost>
2016-01-06 12:51:53 +0100

    commit to test travis

--- src/main/scala/ScalaHelloAction.scala
+++ src/main/scala/ScalaHelloAction.scala
@@ -1,8 +1,9 @@
 import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent, CommonDataKeys}
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.ui.Messages.showMessageDialog
 
 class ScalaHelloAction extends AnAction {
   override def actionPerformed(e: AnActionEvent): Unit = {
-    Messages.showMessageDialog(e.getData(CommonDataKeys.PROJECT), "Hello from Scala", "title", Messages.getInformationIcon())
+    showMessageDialog(e.getData(CommonDataKeys.PROJECT), "Hello from Scala", "title", Messages.getInformationIcon())
   }
 }
