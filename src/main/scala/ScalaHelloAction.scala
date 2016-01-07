36044db9ec1b19f981ff93355e4f8880eb2ec26d
mariusz <mariusz@localhost>
2016-01-07 21:20:44 +0100

    Cleanup

--- src/main/scala/ScalaHelloAction.scala
+++
@@ -1,8 +0,0 @@
-import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent, CommonDataKeys}
-import com.intellij.openapi.ui.Messages
-
-class ScalaHelloAction extends AnAction {
-  override def actionPerformed(e: AnActionEvent): Unit = {
-    Messages.showMessageDialog(e.getData(CommonDataKeys.PROJECT), "Hello from Scala", "title", Messages.getInformationIcon())
-  }
-}
